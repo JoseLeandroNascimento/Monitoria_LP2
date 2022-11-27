218c5dfa156e155f2ffe957d0cb388abf0f3932f
j.leandro1998silva@gmail.com <j.leandro1998silva@gmail.com>
2022-11-27 11:23:56 -0500

    comentário da questão 2

--- Lista_funcoes_geradoras_exceptions/questao2.py
+++ Lista_funcoes_geradoras_exceptions/questao2.py
@@ -4,18 +4,27 @@ import random
 
 def sorteadorMegaSena(quantidade):
 
+    # Quarda referência dos número já sorteados
+    # Evitando que o mesmo seja sorteado novamente
     numeros_sorteados = []
 
+    # Conta a quantidade de números sorteados
     cont = 0
+
     while cont <= quantidade:
 
+        # Gera um número aletóario entre 1 - 60
         numero = random.randint(1,60)
 
+        # Verifica se o número gerado já foi sorteado
         if numero is not numeros_sorteados:
 
+            # Caso o número seja valido, é contado um número gerado
+            # e o mesmo é adicionado na lista do números sorteados
             cont+=1
             numeros_sorteados.append(numero)
-
+            
+            # Retorna o valor gerado
             yield numero
 
 
